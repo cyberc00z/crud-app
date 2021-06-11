91de71d2b2b9bf3086118770b36acfab5115816c
imadhrit <maharajrabel@gmail.com>
2021-06-11 12:54:58 +0530

    form module for editing

---
+++ src/components/FormField.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {View, TextInput,Text} from "react-native";
+
+
+const FormField = ({label, textContentType ,value, multiline, onChangeText}) => (
+  
+   <View
+     style={{
+      borderBottomWidth: 1,
+      borderBottomColor: "dcdcdc",
+      marginBottom: 15
+     }}
+   >
+    <Text style={{marginBottom: 12, fontSize: 14, color:"#C5C5C5"}}>{label}</Text>
+     <TextInput
+     value={value}
+     style={{
+         fontSize:13,
+         fontWeight: "500",
+         color:"#364047",
+         paddingBottom:5
+     }}
+     textContentType={textContentType}
+     multiline={multiline}
+     onChangeText={onChangeText}
+     underlineColorAndroid="transparent"
+     /> 
+   </View>
+);
+
+export default FormField;
