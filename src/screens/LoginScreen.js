bc87d5a63ebc32c71c1f23f06c59ab119274ec2a
imadhrit <maharajrabel@gmail.com>
2021-06-18 07:29:15 +0530

    login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,47 +1,55 @@
 import React, { useState , useEffect} from 'react'
-import {Input,Button} from "react-native-elements";
-import {View,StyleSheet,KeyboardAvoidingView,Platform, Keyboard,Text} from "react-native";
-import {StatusBar} from "expo-status-bar";
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import {Button,Input,Block, theme} from "galio-framework";
+import {View,Text,StyleSheet,KeyboardAvoidingView,Platform, Keyboard, ToastAndroid} from "react-native";
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { auth } from '../utils/firebase';
 
+
 const LoginScreen = ({navigation})  => {
-
     const [email, setEmail] = useState('');
-    const [passowrd, setPassword] = useState('');
-
+    const [password, setPassword] = useState('');
+    
     useEffect(() => {
         const cleanup = auth.onAuthStateChanged((authUser) => {
+            console.log(authUser)
             if (authUser){
-                navigation.navigate('Home');
+                 navigation.replace('BottomTabNavigator');
             }
         });
-        return cleanup
+        return cleanup;
     }, [])
 
     const signIn = () => {
-     console.log("Look at this bro!");
-     navigation.navigate('Home')
+        auth.signInWithEmailAndPassword(email,password)
+        .catch((error) => ( 
+            JSON.stringify(error),
+            ToastAndroid.showWithGravity(
+                error.message,
+                ToastAndroid.SHORT,
+                ToastAndroid.CENTER,
+            )
+        ));
     }
     return (
           <KeyboardAvoidingView
            behavior={Platform.OS==='android' ? "padding": "height"}
            style={styles.container}
           >
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>       
-            <View style={styles.inner}>
-                <Input placeholder="Email" autoFocus type="email"  />
-                <Input placeholder="Password" autoFocus type="password" secureTextEntry />
-
-            <Button contentStyle={styles.button}  title="Login" onPress={signIn}   />
-            <Text onPress={()=>navigation.navigate('Forget_Password')} style={styles.text} >
-                Forget Password ? 
-            </Text>
-            <Button onPress={() => navigation.navigate('Register')} contentStyle={styles.button}  title="Register" type="outline"   />
-
+            <Block style={styles.inner}>
+                <Input  placeholder="Email" type="email-address" autoFocus value={email}   onChangeText={(text) =>setEmail(text)} color={theme.COLORS.BLACK} style={{borderColor: theme.COLORS.INFO, fontSize:30}} placeholderTextColor={theme.COLORS.BLACK}  />
+                <Input placeholder="Password" type="visible-password"   placeholderTextColor={theme.COLORS.BLACK} value={password} onChangeText={(text) => setPassword(text)} color={theme.COLORS.BLACK} onSubmitEditing={signIn} scrollEnabled />
+           <TouchableWithoutFeedback onPress={Keyboard.dismiss}>   
+            <Button contentStyle={styles.button} onPress={signIn}  type="solid" capitalize shadowColor shadowless >Login</Button>
+           </TouchableWithoutFeedback>
+            {/*<TouchableWithoutFeedback onPress={Keyboard.dismiss}>   
+            <Button title="Forget Password ?" onPress={()=>navigation.navigate('ForgetPassword')} style={styles.text} type="clear" />
+    </TouchableWithoutFeedback>*/}
+             <TouchableOpacity onPress={()=>navigation.navigate("Register")}>
+                 <Text style={styles.text} >First Time? Create an Account </Text>
+             </TouchableOpacity>
             <View style={{height: 100}} />
-          </View>
-          </TouchableWithoutFeedback>
+          </Block>
+
         </KeyboardAvoidingView>
     )
 }
@@ -54,19 +62,21 @@ const styles = StyleSheet.create({
         alignItems:"center",
         justifyContent:"center",
         padding:10,
-        backgroundColor:"white"
+        backgroundColor:"#000"
     },
     inner: {
         width:300,
-        marginTop:200,
+        marginTop:220,
+        
     },
     button: {
         width:200,
         marginTop: 10,
+        marginLeft:200,
     },
     text : {
-       color:'#2C6BED',
+       color:theme.COLORS.WARNING,
        textAlign:'center',
-       fontSize: 15
+       fontSize: 13
     }
 })
