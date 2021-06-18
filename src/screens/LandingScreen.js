0cb3e7db2fc4f3ac55ae7358dec455d043f43cd5
imadhrit <maharajrabel@gmail.com>
2021-06-18 07:33:44 +0530

    landing screen

---
+++ src/screens/LandingScreen.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, StatusBar, Dimensions } from 'react-native';
+import { Block, Button ,Text, theme } from 'galio-framework';
+
+
+const { height, width } = Dimensions.get('screen');
+
+import materialTheme from '../constants/Theme';
+import Images from '../constants/Images';
+
+
+export const navigationOptions = ({navigation}) => ({
+  title: "Trantor",
+  headerTitleStyle:{
+    fontWeight:"500",
+    fontSize: 20,
+    textAlign:"center",
+  },  
+})
+
+export default class LandingScreen extends React.Component {
+  componentDidMount(){
+    this._navListener= this.props.navigation.addListener("didFocus",  () => {
+      StatusBar.setBarStyle("light-content");
+    })
+  }  
+  render(){
+        return (
+            <Block flex style={styles.container}>
+                <StatusBar barStyle="light-content" />
+        <Block flex center>
+          <ImageBackground
+            source={{  uri: Images.LandingImage }}
+            style={{ height: height, width: width, marginTop: '-55%', zIndex: 1 }}
+          />
+        </Block>
+        <Block flex space="between" style={styles.padded}>
+          <Block flex space="around" style={{ zIndex: 2 }}>
+            <Block>
+              <Block>
+                <Text color="white" size={60}>Material</Text>
+              </Block>
+              <Block row>
+                <Text color="white" size={60}>Kit</Text>
+              </Block>
+              <Text size={16} color='rgba(255,255,255,0.6)'>
+                Fully coded React Native components.
+              </Text>
+            </Block>
+            <Block center>
+              <Button
+                shadowless
+                style={styles.button}
+                color={materialTheme.COLORS.BUTTON_COLOR}
+                onPress={() => navigation.navigate('Register')}>
+                GET STARTED
+              </Button>
+            </Block>
+          </Block>
+        </Block> 
+            </Block>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "black",
+    },
+    padded: {
+        paddingHorizontal: theme.SIZES.BASE * 2,
+        position: 'relative',
+        bottom: theme.SIZES.BASE,
+    },
+    button: {
+        width: width - theme.SIZES.BASE * 4,
+        height: theme.SIZES.BASE * 3,
+        shadowRadius: 0,
+        shadowOpacity: 0,
+    },
+})
