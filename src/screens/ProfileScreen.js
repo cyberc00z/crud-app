6e932294d67cb0926f0fda667987335d7c7dffff
imadhrit <maharajrabel@gmail.com>
2021-06-20 19:58:30 +0530

    profile carding

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,27 +1,156 @@
+//import { Icon , Button, Text } from "native-base"
 import React from "react";
-import {ScrollView, Text} from "react-native";
-import { Button } from "react-native-elements";
+import {ImageBackground,ScrollView, StyleSheet,Platform, Dimensions,Image } from "react-native";
+import {Block, theme, Text} from "galio-framework";
+import bg from "../images/assets/profileBack.png";
+import { materialTheme,Images } from "../constants/Index";
+import {HeaderHeight} from "../constants/utils";
+import {LinearGradient} from "expo-linear-gradient";
+import Counter from "../components/Counter";
+import { Avatar } from "react-native-elements";
+import { ListItem } from "native-base";
+
+const {width, height} = Dimensions.get("screen");
+
 
 export const navigationOptions = ({ navigation }) => ({
-   headerStyle: {
-       backgroundColor: "transparent",
-       borderBottomColor: "transparent",
-    },
-    headerRight: () => (
-       <Button onPress={() => navigation.navigate("EditProfile")} style={{marginRight: 10}} title="Edit Profile" />
-    ),
-    headerTransparent: true,
-    headerBackTitle: null
+    headerShown: false
 })
 
 export default class ProfileScreen extends React.Component{
     render(){
         return (
-            <ScrollView style={{flex:1}} >
-              <Text>
-                  This is Profile 
-              </Text>
-            </ScrollView>
+           <Block flex style={styles.profile}>
+              <Block flex>
+          <ImageBackground
+            source={bg}
+            style={styles.profileContainer}
+            imageStyle={styles.profileImage}>
+          
+           <Block flex style={styles.profileDetails}>
+               <Avatar source={{ uri : 'https://source.unsplash.com/random'}} rounded size={150}  />
+           
+                <Block style={styles.profileTexts}>
+                 <Text color="white" size={28} style={{paddingBottom: 8}}>R2D2</Text>
+                 <Block row space="between">
+                   <Block row>
+                     <Block middle style={styles.school}>
+                      <Text size={16} color="white">VIT</Text>
+                     </Block>
+                    
+                   </Block>
+                </Block>
+              </Block>
+             <LinearGradient colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,0.1)']} style={styles.gradient} />
+           </Block>
+          </ImageBackground>
+         </Block>
+           <Block flex style={styles.options}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Block row space="between" style={{ padding: theme.SIZES.BASE, }}>
+            <Block middle>
+                <Text bold size={12}  >
+                    <Counter style={styles.statSection}  number="4">Dicussions</Counter> 
+                </Text>
+              </Block>
+              <Block middle>
+                <Text bold size={12} style={{marginBottom: 8}}>
+                  <Counter number={11}>
+                      Old
+                  </Counter>
+                </Text>
+              </Block>
+              <Block middle>
+                <Text bold size={12} style={{marginBottom: 8}}>
+                  <Counter number={44}>
+                     Points
+                  </Counter>
+                </Text>
+              </Block>
+            </Block>
+            <Block style={{ paddingBottom: -HeaderHeight * 6 }}>
+              <Block row space="between" style={{ flexWrap: 'wrap' }} >
+              
+              </Block>
+            </Block>
+          </ScrollView>
+        </Block>
+           </Block>          
         )
     }
 }
+
+const margin = 15;
+
+const styles = StyleSheet.create({
+    profile: {
+        marginTop: Platform.OS === "android" ? -HeaderHeight :  0,
+        marginBottom: -HeaderHeight * 2,
+     },
+     profileImage: {
+       width: width * 1.1,
+       height:'auto',
+     },
+     profileContainer: {
+       width: width,
+       height: height / 2,
+     },
+     profileDetails: {
+       paddingTop: theme.SIZES.BASE * 4,
+       justifyContent: 'space-between',
+       position: 'relative',
+       flexDirection:"row",
+       marginTop: 35 + "%",
+       marginLeft:10 + "%"
+    },
+     profileTexts: {
+       paddingHorizontal: theme.SIZES.BASE * 2,
+       paddingVertical: theme.SIZES.BASE * 2,
+       zIndex: 2
+     },
+     school: {
+       backgroundColor: materialTheme.COLORS.LABEL,
+       paddingHorizontal:6,
+       marginRight: theme.SIZES.BASE /2,
+       borderRadius: 4,
+       height: 19,
+       width: 38,
+     },
+     gradient: {
+        zIndex:1,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: '50%',
+        position: 'absolute',
+     },
+     stats:{
+        flexDirection:"column",
+        marginLeft:30,
+        marginRight:30,
+         
+     },
+     statSection: {
+        paddingLeft: margin * 2,
+        paddingRight: margin,
+        flexDirection: "column" ,
+        backgroundColor: materialTheme.COLORS.LABEL,
+     },
+     options: {
+      position: 'relative',
+      padding: theme.SIZES.BASE,
+      marginHorizontal: theme.SIZES.BASE,
+      marginTop: -theme.SIZES.BASE * 7,
+      borderTopLeftRadius: 13,
+      borderTopRightRadius: 13,
+      backgroundColor: theme.COLORS.WHITE,
+      shadowColor: 'black',
+      shadowOffset: { width: 0, height: 0 },
+      shadowRadius: 8,
+      shadowOpacity: 0.2,
+      zIndex: 2,  
+    }
+
+
+
+})
