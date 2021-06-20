81311057e0ae553036a5c6e524a4b7a3c2334450
imadhrit <maharajrabel@gmail.com>
2021-06-20 19:59:01 +0530

    login screen final

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -8,11 +8,13 @@ import { auth } from '../utils/firebase';
 const LoginScreen = ({navigation})  => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
     
     useEffect(() => {
-        const cleanup = auth.onAuthStateChanged((authUser) => {
-            console.log(authUser)
-            if (authUser){
+        const cleanup = auth.onAuthStateChanged((user) => {
+            setUser(user)
+            //console.log(user)
+            if (user){
                  navigation.replace('BottomTabNavigator');
             }
         });
@@ -22,6 +24,7 @@ const LoginScreen = ({navigation})  => {
     const signIn = () => {
         auth.signInWithEmailAndPassword(email,password)
         .catch((error) => ( 
+            console.log(error),
             JSON.stringify(error),
             ToastAndroid.showWithGravity(
                 error.message,
@@ -36,20 +39,17 @@ const LoginScreen = ({navigation})  => {
            style={styles.container}
           >
             <Block style={styles.inner}>
+            <Text style={{color:"white", top:-100, fontSize:25,textAlign:"center" }}>Login To Spancer</Text>
+               
                 <Input  placeholder="Email" type="email-address" autoFocus value={email}   onChangeText={(text) =>setEmail(text)} color={theme.COLORS.BLACK} style={{borderColor: theme.COLORS.INFO, fontSize:30}} placeholderTextColor={theme.COLORS.BLACK}  />
                 <Input placeholder="Password" type="visible-password"   placeholderTextColor={theme.COLORS.BLACK} value={password} onChangeText={(text) => setPassword(text)} color={theme.COLORS.BLACK} onSubmitEditing={signIn} scrollEnabled />
            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>   
-            <Button contentStyle={styles.button} onPress={signIn}  type="solid" capitalize shadowColor shadowless >Login</Button>
+            <Button style={styles.button} onPress={signIn}  type="solid" capitalize shadowColor shadowless>Login</Button>
            </TouchableWithoutFeedback>
-            {/*<TouchableWithoutFeedback onPress={Keyboard.dismiss}>   
-            <Button title="Forget Password ?" onPress={()=>navigation.navigate('ForgetPassword')} style={styles.text} type="clear" />
-    </TouchableWithoutFeedback>*/}
-             <TouchableOpacity onPress={()=>navigation.navigate("Register")}>
+           <TouchableOpacity onPress={()=>navigation.navigate("Register")}>
                  <Text style={styles.text} >First Time? Create an Account </Text>
-             </TouchableOpacity>
-            <View style={{height: 100}} />
+             </TouchableOpacity>   
           </Block>
-
         </KeyboardAvoidingView>
     )
 }
@@ -64,6 +64,7 @@ const styles = StyleSheet.create({
         padding:10,
         backgroundColor:"#000"
     },
+    
     inner: {
         width:300,
         marginTop:220,
@@ -72,10 +73,10 @@ const styles = StyleSheet.create({
     button: {
         width:200,
         marginTop: 10,
-        marginLeft:200,
+        marginLeft:45,
     },
     text : {
-       color:theme.COLORS.WARNING,
+       color:theme.COLORS.TWITTER,
        textAlign:'center',
        fontSize: 13
     }
