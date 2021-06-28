44e8e4d20da27844932b1fe3fc89895fc9467a7a
imadhrit <maharajrabel@gmail.com>
2021-06-29 03:09:38 +0530

    bottom navigaiton

--- src/components/BottomTabNavigator.js
+++ src/components/BottomTabNavigator.js
@@ -5,9 +5,11 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {  Block } from "galio-framework";
 import HomeScreen, { navigationOptions as homeNavigationOptions } from "../screens/HomeScreen";
 import SearchScreen, { navigationOptions as searchNavigationOptions } from "../screens/SearchScreen";
+import AddPostScreen, {navigationOptions as postNavigationOptions} from "../screens/AddPostScreen";
 import NotificationsScreen, { navigationOptions as notificationsNavigationOptions } from "../screens/NotificationsScreen";
 import ProfileScreen, { navigationOptions as profileNavigationOptions } from "../screens/ProfileScreen";
 import { BottomTabs } from "./BottomTabs";
+
 
 // Creating separate stacks to be able to configure header via navigationOptions.
 // There is no way to configure header for bottom tabs otherwise.
@@ -50,6 +52,18 @@ const Search = () => (
   </SearchStack.Navigator>
 );
 
+const Post = () => (
+  <PostStack.Navigator>
+       <PostStack.Screen
+         component={AddPostScreen}
+         name="AddPost"
+         options={postNavigationOptions}
+       />
+  </PostStack.Navigator>
+ 
+)
+
+
 const Home = () => (
   <HomeStack.Navigator>
     <HomeStack.Screen
@@ -66,7 +80,8 @@ export const BottomTabNavigator = () => (
     <Block flex >
       <BottomTab.Navigator tabBar={props => <BottomTabs {...props} />}>
         <BottomTab.Screen component={Home} name="Home" />
-        <BottomTab.Screen component={Search} name="Search" /> 
+        <BottomTab.Screen component={Search} name="Search" />
+        <BottomTab.Screen component={Post} name="Post" /> 
         <BottomTab.Screen component={Notifications} name="Notifications" />
         <BottomTab.Screen component={Profile} name="Profile" />
       </BottomTab.Navigator>
