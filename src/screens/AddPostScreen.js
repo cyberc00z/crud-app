cf565ecad448922f55d22109b432db12e5a0ea4c
imadhrit <maharajrabel@gmail.com>
2021-07-04 12:23:59 +0530

    post

--- src/screens/AddPostScreen.js
+++ src/screens/AddPostScreen.js
@@ -28,6 +28,9 @@ export const navigationOptions = ({navigation}) => ({
 const AddPostScreen = ({navigation}) => {
     const [title,setTitle] = useState("");
     const [desc, setDesc] = useState("");
+    const [upVoteNum, setUpVoteNum] = useState(1);
+    const [downVoteNum , setDownVoteNum] = useState(0);
+    const [commentNum, setCommentNum] = useState(0);
 
     const onPostUpload = () => {
         if (!title){
@@ -45,7 +48,10 @@ const AddPostScreen = ({navigation}) => {
                 desc:desc ,
                 createdBy: auth.currentUser.email,
                 displayName: auth.currentUser.displayName,
-                photoURL: auth.currentUser.photoURL
+                photoURL: auth.currentUser.photoURL,
+                upVoteNum : upVoteNum,
+                commentNum: commentNum,
+                downVoteNum : downVoteNum
             }).then(() => {
                 navigation.goBack();
                 setTitle("");
@@ -98,100 +104,6 @@ const AddPostScreen = ({navigation}) => {
 }
 
 export default AddPostScreen;
-
-/*
-export default class AddPostScreen  extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            title:"",
-            desc: "", 
-
-            isLoading: false
-        };
-    }
-
-    setTitle = (title) => {
-        this.setState({
-            title:title, 
-        }); 
-    };
-    setDesc = (desc) => {
-       this.setState({
-           desc: desc
-       })
-    }
-    
-    onPostUpload = async () => {
-        const {title, desc} = this.state;
-        if (!title){
-            Alert.alert("Cann't Start a Discussion without a Title");
-            return;
-        }  
-        if (!desc){
-            Alert.alert("Need Description...");
-            return;
-        }
-        try {
-            this.setState({
-                isLoading: true,
-            });
-           
-            this.setState({
-                isLoading: false,
-                title: '',
-                desc: ''
-            });
-            this.props.navigation.goBack();
-        } catch (err){
-            console.log(err),
-            ToastAndroid.showWithGravity(
-                err.message,
-                ToastAndroid.SHORT,
-                ToastAndroid.CENTER,
-            )
-        }
-        
-    };
-
-    render(){
-        const {title , desc} = this.state;
-
-    return (
-            <View style={styles.container}>
-                <View style={styles.addPostContent}>
-                <TextInput
-                    value={title}
-                    onChangeText={(title) => this.setTitle(title)}
-                    style={styles.textInputTitle}
-                    placeholder="Title"
-                    autoCapitalize="sentences"
-                    autoFocus={true}
-                    maxLength={80}
-                    numberOfLines={3}
-                    textAlignVertical="top"
-                />
-                <TextInput
-                    value={desc}
-                    onChangeText={(desc)=>this.setDesc(desc)}
-                    style={styles.textInputDesc}
-                    placeholder="Details in 300 words max."
-                    autoCapitalize="sentences"
-                    maxLength={300}
-                    multiline={true}
-                    numberOfLines={8}
-                    textAlignVertical="top"
-                />
-               </View>
-                <TouchableOpacity>
-                <Button style={styles.button} onPress={this.onPostUpload}  type="solid" capitalize shadowColor shadowless>Login</Button>    
-                </TouchableOpacity>     
-            </View>
-            
-        );
-    }
-};
-*/
 
 const styles = StyleSheet.create({
     container: {
