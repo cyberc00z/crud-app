c410f4c5662aa89410b697f51b812719518cbb5e
imadhrit <maharajrabel@gmail.com>
2021-07-04 12:17:38 +0530

    harness og

---
+++ src/harness/spancerProfiles.js
@@ -0,0 +1,36 @@
+/**write a module for assign names to anonymous citizens 
+ *  Names : 
+ * 1. G'Kar
+ * 2. R2D2
+ * 3. HAL-9000
+ * 4. C3P0 
+ *  We have ask user to choose any one names of above written names 
+ * 
+ * 
+ * */ 
+
+export  const G = [
+    {
+       name: "G'kar", 
+       image: " ", 
+    }    
+];
+export const R = [
+    {
+        name: "R2D2",
+        image: " ",
+    }
+];
+export const H = [
+    {
+        name : "HAL-9000",
+        image : " " 
+    
+    }
+];
+export  const C = [
+    {
+        name: "C3P0",
+        image : " "
+    }
+]
