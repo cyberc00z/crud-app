2be3f31cd9941dd79be0f91cdc9077623a7e83a2
imadhrit <maharajrabel@gmail.com>
2021-07-05 11:51:04 +0530

    trending post enabled

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,10 @@
-import React from "react";
-import {Image, View, StatusBar,SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {Image, View, StatusBar,SafeAreaView,StyleSheet } from "react-native";
 import Heading from "../components/Heading";
 import SearchBox from "../components/SearchBox";
-import HorizontalScrollFeed from "../components/HorizontalScrollFeed";
-import GroupCard from "../components/GroupCard";
+import CustomListItem from "../components/CustomListItem";
+//import GroupCard from "../components/GroupCard";
+import {db, auth} from "../utils/firebase";
 
 export const navigationOptions = ({navigation}) => ({
     title:"DISCOVER",
@@ -22,121 +23,60 @@ export const navigationOptions = ({navigation}) => ({
     )
 });
 
+const  SearchScreen = ({navigation}) => {
+  const [trendingPost, setTrendingPost] = useState([]);
+  useEffect(() => {
+    const unsubscribe =db.collection("posts").where("Vote", ">=", 2).orderBy("Vote", "desc").onSnapshot((snapshot) => 
+       setTrendingPost(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data()
+          }))     
+       )  
+    );
+    return unsubscribe
+  },[navigation])
+
+  const expandPost = (id, title, displayName, timestamp, photoURL,desc,commentNum, Vote ) => {
+      navigation.navigate("PostView", {
+        id,
+        photoURL,
+        displayName,
+        timestamp,
+        desc,
+        title,
+        commentNum,
+        Vote
+      })
+  }
+  return (
+    <SafeAreaView style={{flex:1, backgroundColor:"#fff"}}>
+                <SearchBox />
+                <Heading>Trending Discussions</Heading> 
+                {trendingPost.map(({id, data : {
+                 photoURL,displayName,timestamp,desc ,title , commentNum, Vote
+                         }}) => (
+                  <CustomListItem key={id} id={id} photoURL={photoURL} title={title} displayName={displayName} timestamp={timestamp}  desc={desc} commentNum={commentNum} Vote={Vote} expandPost={expandPost} />
+               ))}
+           </SafeAreaView>
+  )
+}
+
+export default SearchScreen;
+
+const styles = StyleSheet.create({
+
+})
+
+/*
 export default class SearchScreen extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-                interestingPosts: [
-                {
-                    id: 1235,
-                    post_image: "",
-                    post:"Why this colavari de ?"
-                  },
-                  {
-                    id: 2345,
-                    post_image: "https://randomuser.me/api/portraits/men/24.jpg",
-                    post: "This is new post "
-                  },
-                  {
-                    id: 3456,
-                    post_image: "https://randomuser.me/api/portraits/women/45.jpg"
-                  },
-                  {
-                    id: 4567,
-                    post_image: "https://randomuser.me/api/portraits/men/45.jpg",
-                    post: "I have to modify data more"
-                  },
-                  {
-                    id: 6789,
-                    post_image: "https://randomuser.me/api/portraits/women/23.jpg",
-                    post:"Why this colavari de ?"  
-                },
-                  {
-                    id: 7890,
-                    post_image: "https://randomuser.me/api/portraits/men/67.jpg",
-                    post:"Why this colavari de ?"  
-                },
-                  {
-                    id: 2456,
-                    post_image: "https://randomuser.me/api/portraits/women/12.jpg",
-                    post:"Why this colavari de ?"
-                  }
-                ],
-                trendingGroups: [
-                  {
-                    id: 1234,
-                    name: "Beer",
-                    image:
-                      "https://cdn.britannica.com/700x450/72/186972-049-26ACDCBE.jpg",
-                    icon: "",
-                    post:"Why this colavari de ?"
-                  },
-                  {
-                    id: 2345,
-                    name: "Arcade",
-                    image: "http://www.thebasementarcade.com/gameroom/0516/1.jpg",
-                    icon: "",
-                    post:"Why this colavari de ?"
-                  },
-                  {
-                    id: 3456,
-                    name: "Nature",
-                    image:
-                      "https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=350",
-                    icon: "",
-                    post:"Why this colavari de ?"
-                  },
-                  { id: 4567, image: "", icon: "" },
-                  { id: 6789, image: "", icon: "" },
-                  { id: 7890, image: "", icon: "" },
-                  { id: 8909, image: "", icon: "" }
-                ],
-                users: [
-                  {
-                    id: 1235,
-                    name: "Danny",
-                    post_image: "https://randomuser.me/api/portraits/women/65.jpg",
-                    post:"Why this colavari de ?"
-                  },
-                  {
-                    id: 2345,
-                    name: "James",
-                    post_image: "https://randomuser.me/api/portraits/men/24.jpg",
-                    post:"Why this colavari de ?" 
-                  },
-                  {
-                    id: 3456,
-                    name: "Jennifer",
-                    post_image: "https://randomuser.me/api/portraits/women/45.jpg",
-                    post:"Why this colavari de ?"
-                  },
-                  {
-                    id: 4567,
-                    name: "hello world",
-                    post_image: "https://randomuser.me/api/portraits/men/45.jpg",
-                    post:"Why this colavari de ?"
-                  },
-                  {
-                    id: 6789,
-                    name: "hello world",
-                    post_image: "https://randomuser.me/api/portraits/women/23.jpg",
-                    post:"Why this colavari de ?"
-                  },
-                  {
-                    id: 7890,
-                    name: "hello world",
-                    post_image: "https://randomuser.me/api/portraits/men/67.jpg",
-                    post:"Why this colavari de ?"
-                  },
-                  {
-                    id: 2456,
-                    name: "hello world",
-                    post_image: "https://randomuser.me/api/portraits/women/12.jpg",
-                    post:"Why this colavari de ?"
-                  }
-            ]
+                interestingPosts: []
         }
-    }
+
+  }
     componentDidMount(){
         this._navListener = this.props.navigation.addListener("didFocus",()=>{
             StatusBar.setBarStyle("dark-content"); 
@@ -163,4 +103,4 @@ export default class SearchScreen extends React.Component{
            </SafeAreaView>
         )
     }
-}
+}*/
