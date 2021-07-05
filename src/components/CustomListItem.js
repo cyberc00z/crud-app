89606abb56e1364a282583a8593a915dc9a6bca6
imadhrit <maharajrabel@gmail.com>
2021-07-05 11:53:26 +0530

    custom list item

--- src/components/CustomListItem.js
+++ src/components/CustomListItem.js
@@ -4,10 +4,10 @@ import { ListItem, Avatar } from "react-native-elements";
 
 
 
-const CustomListItem = ({id, photoURL,displayName,timestamp,desc ,title ,expandPost,commentNum, upVoteNum, downVoteNum }) => {        
+const CustomListItem = ({id, photoURL,displayName,timestamp,desc ,title ,expandPost,commentNum, Vote }) => {        
     return (
       <ScrollView>
-        <ListItem key={id} bottomDivider style={styles.container} onPress={()=>expandPost(id,photoURL,displayName,timestamp,desc ,title, commentNum, upVoteNum, downVoteNum)} >
+        <ListItem key={id} bottomDivider style={styles.container} onPress={()=>expandPost(id,photoURL,displayName,timestamp,desc ,title, commentNum, Vote)} >
             <Avatar
               rounded
               size={50}
@@ -25,11 +25,8 @@ const CustomListItem = ({id, photoURL,displayName,timestamp,desc ,title ,expandP
                 <ListItem.Subtitle style={styles.num}>
                   {commentNum}
                 </ListItem.Subtitle>
-                <ListItem.Subtitle style={styles.num} >
-                  {upVoteNum}
-                </ListItem.Subtitle>
                 <ListItem.Subtitle style={styles.num}>
-                  {downVoteNum}
+                  {Vote}
                 </ListItem.Subtitle>
               <ListItem.Subtitle>
                 posted by @{displayName}
